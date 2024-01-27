1e4b5ac198aaa552757877a56af286bd81273011
Aibar Arpish <aibar201329@gmail.com>
2024-01-27 13:23:39 +0600

    Создал и заполнил массив из строк

---
+++ Task/Program.cs
@@ -0,0 +1,13 @@
+﻿
+string[] strings = AddElements(5);
+
+
+
+string[] AddElements(int size){
+    string[] str = new string[size];
+    for(int i=0; i<str.Length; i++){
+        Console.WriteLine($"Enter the {i+1} string");
+        str[i] = Console.ReadLine();
+    }
+    return str;
+}
